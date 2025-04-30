309c4c25f6f4fe87e72b404044276ef47a928690
yaman3397 <36197870+yaman3397@users.noreply.github.com>
2025-04-30 09:00:03 +0200

    Final Commit

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
+# Required Libraries
+    pip install matplotlib mne mne-bids mne-icalabel autoreject scipy numpy pandas
+
+
 # What is happening
 
-- utils.py: all functions
-    - preprocess_subject: take one subject id and preprocess it (filter frequencies and ICA, so blinks get removed)
+- utils.py: all functions used by the jupyter notebooks
+    - preprocess_subject: take one subject id and preprocess it (filter frequencies)
         - returns a raw instance
-    - create_epochs: takes one raw instance.
+    - run_ica: Run ICA for the given raw instance, then do the final frequency filter (0.1 - 30Hz)
+    - create_evokeds: takes one raw instance
         - create epochs for feedback and cue win/loss
-        - reject peak-to-peak 120µV (doesn't do anything?)
+        - reject peak-to-peak 120µV
+        - Additionally, use autoreject to remove bad epochs
         - plot topographies and frequencies
-        - Write average epochs to files
+        - Write average epochs and evokeds to disk for processing in 02-analysis.ipynb
+    - create_subplot_all_subjects:
+        - Used by 02-analysis.ipynb to show the grand averages per single subject in a single plot
 
-- 01-preprocessing:
+- 01-preprocessing.ipynb:
     - Run preprocessing for all subjects (uses utils.py functions)
-        - So that all files get created
+        - So that all files get created for further processing
 
 - 02-analysis.ipynb:
     - Read all files that were created by eeg-preprocessing
     - Then create the grand average of all 12 subjects
     - Finally, create the graphs
+
+- 03-linear-regression.ipynb:
+    - Read the epochs created in 01-preprocessing.ipynb and train multiple linear models
--- utils.py
+++ utils.py
@@ -52,20 +52,21 @@ def run_ica(raw):
     """
 
     # Requirement for MNE-ICLabel
-    raw.set_eeg_reference('average')
+    ica_raw = raw.copy().filter(l_freq=1.0, h_freq=100.0)
+    ica_raw = ica_raw.set_eeg_reference('average')
     
     # Run ICA
     ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter="auto", method='infomax', fit_params=dict(extended=True))
-    ica.fit(raw)
+    ica.fit(ica_raw)
 
-    ic_labels = label_components(raw, ica, method='iclabel')
+    ic_labels = label_components(ica_raw, ica, method='iclabel')
     exclude_idx = [
         idx for idx, label in enumerate(ic_labels["labels"]) if label not in ["brain", "other"]
     ]
     
     for exclude in exclude_idx:
         explained_var_ratio = ica.get_explained_variance_ratio(
-            raw, components=exclude, ch_type="eeg"
+            ica_raw, components=exclude, ch_type="eeg"
         )
 
         ratio_percent = round(100 * explained_var_ratio["eeg"])
@@ -74,7 +75,7 @@ def run_ica(raw):
             f"{ratio_percent}%"
         )
 
-    explained_var_ratio = ica.get_explained_variance_ratio(raw, ch_type="eeg")
+    explained_var_ratio = ica.get_explained_variance_ratio(ica_raw, ch_type="eeg")
     for channel_type, ratio in explained_var_ratio.items():
         print(f"Fraction of {channel_type} variance explained by all components: {ratio}")
 
@@ -86,10 +87,11 @@ def run_ica(raw):
     ica.plot_components()
     ica.plot_overlay(raw, exclude=exclude_idx);
 
-    ica.apply(raw)
+    ica.apply(raw, exclude=exclude_idx)
 
     # Lowpass filter after ICA
     raw.filter(l_freq=0.1, h_freq=30, method='fir', fir_design='firwin')
+    raw.set_eeg_reference(["TP9", "TP10"])
 
     # noisy_chs = mne.preprocessing.find_bad_channels_lof(raw)
     # print("Removing noisy channels:", noisy_chs)
@@ -114,96 +116,64 @@ def create_evokeds(raw, subject_id):
     
     reject_criteria = dict(eeg=120e-6)  # Add 120 µV threshold for EEG channels
 
-    win_events = {
+    events_relevant = {
+        # win events
         "S  6": "win (low-task, low-cue)",
         "S 16": "win (mid-task, low-cue)",
         "S 26": "win (mid-task, high-cue)",
-        "S 36": "win (high-task, high-cue)"
-    }
-    loss_events = {
+        "S 36": "win (high-task, high-cue)",
+
+        # loss_events
         "S  7": "loss (low-task, low-cue)",
         "S 17": "loss (mid-task, low-cue)",
         "S 27": "loss (mid-task, high-cue)",
         "S 37": "loss (high-task, high-cue)"
     }
 
-    all_win_epochs = {}
-    all_loss_epochs = {}
-
-    all_win_evokeds = {}
-    all_loss_evokeds = {}
-    
-    # raw.save(
-    #     f"fif-files/subject{subject_id}_raw_preprocessed_ica.fif", overwrite=True)
-
+    # All processed epochs and evokeds files are saved in the folder fif-files
     if not os.path.isdir("fif-files"):
         os.mkdir("fif-files")
 
-    for key, event_type in win_events.items():
+    # For all win and loss events, get epochs, reject bad epochs and save the evokeds
+    for key, event_type in events_relevant.items():
         epochs = mne.Epochs(
             raw, events, event_id=event_id[f"Stimulus/{key}"], tmin=tmin, tmax=tmax,
             baseline=(None, 0), reject=reject_criteria, preload=True, reject_by_annotation=True
         )
 
         reject = get_rejection_threshold(epochs, decim=2)
-        print(f"Win epochs rejection threshold for {subject_id} is {reject['eeg']}")
+        print(f"{event_type} epochs rejection threshold for {subject_id} is {reject['eeg']}")
         epochs.drop_bad(reject=reject)
+        evokeds = epochs.average()
 
-        all_win_epochs[event_type] = epochs
-        all_win_evokeds[event_type] = epochs.average()
-
-        all_win_evokeds[event_type].save(
-            f"fif-files/subject{subject_id}_feedback_{event_type.replace(' ', '-')}-ave.fif", overwrite=True)
+        epochs.save(f"fif-files/subject{subject_id}_feedback_{event_type.replace(' ', '-')}-epo.fif", overwrite=True)
+        evokeds.save(f"fif-files/subject{subject_id}_feedback_{event_type.replace(' ', '-')}-ave.fif", overwrite=True)
 
 
-    for key, event_type in loss_events.items():
-        epochs = mne.Epochs(
-            raw, events, event_id=event_id[f"Stimulus/{key}"], tmin=tmin, tmax=tmax,
-            baseline=(None, 0), reject=reject_criteria, preload=True, reject_by_annotation=True
-        )
+def create_subplot_all_subjects(event, subject_ids, plt):
+    """
+    Create a 3x4 or 2x4 ERP plot of all subjects
+    """
 
-        reject = get_rejection_threshold(epochs, decim=2)
-        print(f"Loss epochs rejection threshold for {subject_id} is {reject['eeg']}")
-        epochs.drop_bad(reject=reject)
+    nrows = 3
+    ncols = 4
+    if len(subject_ids) == 8:
+        nrows = 2
 
-        all_loss_epochs[event_type] = epochs
-        all_loss_evokeds[event_type] = epochs.average()
+    fig, axes = plt.subplots(
+        nrows=nrows, ncols=ncols, figsize=(12, 6)
+    )
 
-        all_loss_evokeds[event_type].save(
-            f"fif-files/subject{subject_id}_feedback_{event_type.replace(' ', '-')}-ave.fif", overwrite=True)
+    fig.subplots_adjust(hspace=0.4)
 
+    for i, subject_id in enumerate(subject_ids):
+        evoked = mne.read_evokeds(f"fif-files/subject{subject_id}_feedback_{event.replace(' ', '-')}-ave.fif")
+        evoked[0].set_eeg_reference(["TP9", "TP10"])
+        
+        row = i // ncols
+        col = i % ncols
+        axes[row, col].plot(evoked[0].get_data(picks="FCz")[0])
+        axes[row, col].set_title(f"Subject {subject_id}")
 
-    # Step 7: ERP Analysis
-    # Extract P300 peaks for Cue-locked ERP at FCz
-    # fc_z_index = cue_win_evoked.info['ch_names'].index('FCz')
-    # p300_ch_name, p300_latency, p300_amplitude = cue_win_evoked.get_peak(
-    #     ch_type='eeg', mode='pos', tmin=0.2, tmax=0.4, return_amplitude=True)
-    
-    # print(p300_amplitude)
-    # print(f"P300 Peak at {p300_ch_name}: Amplitude = {p300_amplitude} µV, Latency = {p300_latency} s")
-
-    # cue_win_evoked.plot(titles="Cue-Win-Locked ERP");
-    # cue_loss_evoked.plot(titles="Cue-Loss-Locked ERP")
-    # feedback_evoked.plot(titles="Feedback-Locked ERP");
-
-
-    # for epoch in win_epochs:
-    #     # win_epochs[epoch].average().plot(picks="FCz", titles=epoch)
-    #     all_wins[epoch] = win_epochs[epoch].average() #.plot(picks="FCz", titles=epoch)
-
-
-    # all_losses = {}
-    # for epoch in loss_epochs:
-    #     # print(epoch)
-    #     # win_epochs[epoch].average().plot(picks="FCz", titles=epoch)
-    #     all_losses[epoch] = loss_epochs[epoch].average() #.plot(picks="FCz", titles=epoch)
-
-
-    # cue_win_evoked.plot_topomap()
-    # cue_loss_evoked.plot_topomap()
-    # feedback_evoked.plot_topomap()
-
-    # cue_loss_evoked.plot_joint()
-
-    # mne.viz.plot_compare_evokeds(all_wins, picks="FCz")
-    # mne.viz.plot_compare_evokeds(all_losses, picks="FCz")
+    plt.suptitle(event + " for all subjects (FCz)")
+    plt.show()
